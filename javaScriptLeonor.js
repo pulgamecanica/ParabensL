7139e6deb6053a170d879daa308a54c95cd86832
pulgamecanica <pulgamecanica11@gmail.com>
2020-06-13 17:51:33 +0100

    BG change color

--- javaScriptLeonor.js
+++ javaScriptLeonor.js
@@ -2,3 +2,16 @@ var blink = document.getElementById('blink');
 setInterval(function() {
    blink.style.opacity = (blink.style.opacity == 0 ? 1 : 0);
 }, 1000); 
+
+function myFunction() {
+    var x = document.getElementsByTagName("BODY")[0];
+    if (x.style.backgroundColor === 'red') {
+        x.style.backgroundColor = 'blue';
+    }else if(x.style.backgroundColor === 'blue'){ 
+    	x.style.backgroundColor = 'gray';
+    }else if(x.style.backgroundColor == 'gray'){ 
+    	x.style.backgroundColor = 'green';
+    }else {
+        x.style.backgroundColor = 'red';
+    }
+}
